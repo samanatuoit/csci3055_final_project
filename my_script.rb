f6276823d16b056efb3761001c631c16b3c98dda
samanatuoit <saman.yaghini@uoit.net>
2016-12-11 23:51:22 -0500

    Working on bubblesort

--- my_script.rb
+++ my_script.rb
@@ -20,3 +20,25 @@ def square(x)
 end
 
 puts square(5)
+
+
+def bubblesort()
+
+  list = Array[4, 5, 2, 1, 3]
+
+  temp = 0
+  for i in 0..list.length
+    #puts "#{list[i]}"
+    for j in 0..list.length
+     if list[j] > list[i]
+       temp = list[j]
+       list[j] = list[i]
+       list[i] = temp
+     end
+    end
+  end
+  puts "#{list}"
+end
+
+bubblesort
+# bubblesort(numbers)
